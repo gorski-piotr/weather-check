c8c2a9ea4e7fe11b26cd896f12353c719f1f9401
Piotr Gorski <gorskipiotr5@gmail.com>
2021-05-23 21:24:44 +0100

    Search form added

--- src/components/MainFeed.js
+++ src/components/MainFeed.js
@@ -5,6 +5,11 @@ function MainFeed() {
   //useState hook for weather data (initial value = empty array. We will update it later (when we fetch the data from API))
   const [weatherData, setWeatherData] = useState([]);
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   //useEffect with empty array as a second parameter will run the code only once (onMount)
   useEffect(() => {
     // fetch data from weather API
@@ -15,7 +20,7 @@ function MainFeed() {
       .then((res) => res.json())
       .then((resJSON) => {
         //show in the console if we received the data from the API
-        console.log("resJSON", resJSON);
+        // console.log("resJSON", resJSON);
         //updating the state of weatherData
         setWeatherData(resJSON);
       })
@@ -26,34 +31,47 @@ function MainFeed() {
   }, []);
 
   //show in the consol if weatherData updated
-  console.log("weatherData", weatherData);
+  // console.log("weatherData", weatherData);
 
   return (
-    <div className="grid grid-flow-row sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto bg-gray-600">
-      {weatherData.map(
-        ({
-          id_stacji,
-          stacja,
-          data_pomiaru,
-          godzina_pomiaru,
-          temperatura,
-          suma_opadu,
-          cisnienie,
-          wilgotnosc_wzgledna,
-        }) => (
-          <WeatherStation
-            //passing props to the WeatherStation component
-            key={id_stacji}
-            city={stacja}
-            date={data_pomiaru}
-            time={godzina_pomiaru}
-            temp={temperatura}
-            precipitation={suma_opadu}
-            pressure={cisnienie}
-            humidity={wilgotnosc_wzgledna}
-          />
-        )
-      )}
+    <div>
+      {/* search input */}
+      <div className="mt-4">
+        <span>Search city in Poland: </span>
+        <input type="text" value={searchTerm} onChange={handleChange} />
+      </div>
+
+      {/* grid with all the Weather Stations  */}
+      <div className="grid grid-flow-row sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto bg-gray-600">
+        {weatherData
+          .filter((val) => {
+            return val.stacja.toLowerCase().includes(searchTerm.toLowerCase());
+          })
+          .map(
+            ({
+              id_stacji,
+              stacja,
+              data_pomiaru,
+              godzina_pomiaru,
+              temperatura,
+              suma_opadu,
+              cisnienie,
+              wilgotnosc_wzgledna,
+            }) => (
+              <WeatherStation
+                //passing props to the WeatherStation component
+                key={id_stacji}
+                city={stacja}
+                date={data_pomiaru}
+                time={godzina_pomiaru}
+                temp={temperatura}
+                precipitation={suma_opadu}
+                pressure={cisnienie}
+                humidity={wilgotnosc_wzgledna}
+              />
+            )
+          )}
+      </div>
     </div>
   );
 }
